aaf324cd6f4285b6641c68aeeff279457cf3c1fa
drelluiz <andrejsluiz@gmail.com>
2019-10-21 16:47:40 -0300

    Configuração De settings.py

--- siseventos/settings.py
+++ siseventos/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps de terceiros
+    ####
+    # Apps do sistema
+    #'funcionario',
+
 ]
 
 MIDDLEWARE = [
