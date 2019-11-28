2dc9dd690605ea98e1dc62f34282333c5e4004d3
drelluiz <andrejsluiz@gmail.com>
2019-11-27 22:04:43 -0300

    Config

--- siseventos/settings.py
+++ siseventos/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-#ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['127.0.0.1', default=[], cast=Csv()]
+#ALLOWED_HOSTS = '*'
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[''], cast=Csv())
 
 
 # Application definition
