ecec0e893a510ceff349bcadd6542f0cc9b14dd9
drelluiz <andrejsluiz@gmail.com>
2019-11-28 00:50:22 -0300

    settings

--- siseventos/settings.py
+++ siseventos/settings.py
@@ -131,13 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
- 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
