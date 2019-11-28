082ce4d06f4514d714ca62ebf26b88a82506da00
drelluiz <andrejsluiz@gmail.com>
2019-11-27 22:43:25 -0300

    config

--- siseventos/settings.py
+++ siseventos/settings.py
@@ -138,6 +138,7 @@ STATICFILES_DIRS = [
 ]
  
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
