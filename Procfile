894b6aa948326371372ac53cf21b5a86cfed9a90
drelluiz <andrejsluiz@gmail.com>
2019-11-27 21:56:09 -0300

    Procfile & Runtime

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:gunicorn siseventos.wsgi --log-file -
--- siseventos/settings.py
+++ siseventos/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from decouple import config, Csv
-from dj database_url import parse as dburl
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
